67c5e06ac9d3f5261322a0b625ef428e80827a48
cook1e <vincent.schweiger@xolley.de>
2021-08-14 18:28:52 +0200

    initial

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/file.controller");
+
+let routes = (app) => {
+  router.post("/upload", controller.upload);
+  router.get("/files", controller.getListFiles);
+  router.get("/files/:name", controller.download);
+  
+  router.get("/log/:name", controller.log);
+
+  app.use(router);
+};
+
+module.exports = routes;
